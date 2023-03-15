1a0eeaf5ef0336deaae1ae9d3037546fcc716cd3
Freddy Perez <fred.perez15@gmail.com>
2023-03-15 15:29:56 +0200

    event handling lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+const EyesOnMe = () => {
+    const handleOnFocus = () => {
+        console.log('Good!')
+    }
+
+    const handleOnBlur = () => {
+        console.log('Hey! Eyes on me!')
+    }
+    return(
+        <div>
+            <button onFocus={handleOnFocus} onBlur={handleOnBlur}>Eyes on me</button>
+
+        </div>
+    )
+}
+
+export default EyesOnMe;
